26030629b8ce3a4b7e2624f08d279acaa80f3820
alina <alinaaziz831@gmail.com>
2025-01-22 11:09:19 +0500

    updating

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "your_project_id",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "anne0qic",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   useCdn: false,
   apiVersion: "2025-01-12",
